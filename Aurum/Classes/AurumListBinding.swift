b9dda00b5bd88126fe722b08b595deb508d0a00e
Nikita Arkhipov <narhipov92@gmail.com>
2020-02-21 12:53:19 +0300

    List improvement

--- Aurum/Classes/AurumListBinding.swift
+++ Aurum/Classes/AurumListBinding.swift
@@ -9,57 +9,47 @@ import UIKit
 import ReactiveKit
 import Bond
 
-public class AurumListActionListener{
-    typealias ActionListener = (IndexPath) -> Void
+public protocol AurumListConnector: class{
+    associatedtype Data: Equatable
+    associatedtype Action: AurumAction
+    typealias Reducer = Subject<Action, Never>
+    typealias ActionListener = (Int) -> Void
     
-    let listener: ActionListener
+    var reducer: Reducer? { get set }
+    var actionListener: ActionListener? { get set }
+    var reloadList: (() -> Void)? { get set }
     
-    init(actionListener: @escaping ActionListener) {
-        self.listener = actionListener
-    }
+    func update(with: [Data])
 }
 
-public protocol AurumListConnector: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    associatedtype DataType: Equatable
-    associatedtype CellType: UICollectionViewCell
-        
-    func setup(cell: CellType, data: DataType, array: [DataType], indexPath: IndexPath)
-    func update(with: [DataType])
-}
+public protocol AurumCollectionConnector: AurumListConnector, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{ }
 
-public class AurumListBaseConnector<D: Equatable, C: UICollectionViewCell>: NSObject, AurumListConnector{
-    typealias Setuper = (C, D, [D], IndexPath) -> Void
+public protocol AurumTableConnector: AurumListConnector, UITableViewDelegate, UITableViewDataSource{ }
 
-    let setuper: Setuper    
+public class AurumCollectionBaseConnector<Model: Equatable, Cell: UICollectionViewCell, Action: AurumAction>: NSObject, AurumCollectionConnector{
+    typealias Setuper = (Cell, Model, [Model], Int, Reducer) -> Void
+
+    let setuper: Setuper
     let size: CGSize
     
-    var items: [D] = []
+    var items: [Model] = []
+    
+    public var reducer: Subject<Action, Never>?
+    public var reloadList: (() -> Void)?
+    public var actionListener: ActionListener?
 
     init(size: CGSize, setuper: @escaping Setuper) {
         self.size = size
         self.setuper = setuper
     }
     
-    public func cell(collection: UICollectionView, at: IndexPath) -> UICollectionViewCell {
-        let cellType = "\(type(of: CellType.self))"
-        let id = String(cellType.prefix(upTo: cellType.firstIndex(of: ".")!))
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id, for: at) as! CellType
-        let model = items[at.item]
-        setup(cell: cell, data: model, array: items, indexPath: at)
-        return cell
-    }
-
-    public func setup(cell: C, data: D, array: [D], indexPath: IndexPath){
-        setuper(cell, data, array, indexPath)
-    }
-
-    public func update(with: [D]){
+    public func update(with: [Model]){
         items = with
-        collectionView.reloadData()
+        reloadList?()
     }
     
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        actionListener?.listener(indexPath)
+        actionListener?(indexPath.item)
     }
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -67,7 +57,13 @@ public class AurumListBaseConnector<D: Equatable, C: UICollectionViewCell>: NSOb
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return cell(collection: collectionView, at: indexPath)
+        let name = "\(type(of: Cell.self))"
+        let id = String(name.prefix(upTo: name.firstIndex(of: ".")!))
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id, for: indexPath) as! Cell
+        cell.reactive.bag.dispose()
+        let model = items[indexPath.item]
+        if let reducer = reducer { setuper(cell, model, items, indexPath.item, reducer) }
+        return cell
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
@@ -75,112 +71,159 @@ public class AurumListBaseConnector<D: Equatable, C: UICollectionViewCell>: NSOb
     }
 }
 
-public class AurumListBinding<S: AurumState, LC: AurumListConnector, A: AurumAction>: AurumBinding<S, A>{
-    typealias Extractor = (S) -> [LC.DataType]
-    typealias ActionProvider = (IndexPath) -> A?
+public class AurumTableBaseConnector<Model: Equatable, Cell: UITableViewCell, Action: AurumAction>: NSObject, AurumTableConnector{
+    typealias Setuper = (Cell, Model, [Model], Int, Reducer) -> Void
+
+    let setuper: Setuper
+    
+    var items: [Model] = []
+    
+    public var reducer: Subject<Action, Never>?
+    public var reloadList: (() -> Void)?
+    public var actionListener: ActionListener?
+
+    init(setuper: @escaping Setuper) {
+        self.setuper = setuper
+    }
+    
+    public func update(with: [Model]){
+        items = with
+        reloadList?()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let name = "\(type(of: Cell.self))"
+        let id = String(name.prefix(upTo: name.firstIndex(of: ".")!))
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath) as! Cell
+        cell.reactive.bag.dispose()
+        let model = items[indexPath.row]
+        if let reducer = reducer { setuper(cell, model, items, indexPath.row, reducer) }
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        actionListener?(indexPath.row)
+    }
+}
+
+public class AurumListBinding<S: AurumState, LC: AurumListConnector>: AurumBinding<S, LC.Action>{
+    typealias Extractor = (S) -> [LC.Data]
+    typealias ActionProvider = (Int) -> LC.Action?
 
     let extractor: Extractor
-    let component: UICollectionView
     let connector: LC
     var action: ActionProvider?
 
-    init(extractor: @escaping Extractor, component: UICollectionView, connector: LC) {
+    init(extractor: @escaping Extractor, connector: LC) {
         self.extractor = extractor
-        self.component = component
         self.connector = connector
     }
 
-    public override func setup(state: Property<S>, reduce: Subject<A, Never>) {
-        component.delegate = connector
-        component.dataSource = connector
-
+    public override func setup(state: Property<S>, reduce: Subject<LC.Action, Never>) {
+        connector.reducer = reduce
         _ = state.map(extractor).removeDuplicates().observeNext(with: connector.update)
 
         if let action = action{
-            connector.set { ip in
-                if let a = action(ip){ reduce.next(a) }
+            connector.actionListener = { i in
+                if let a = action(i){ reduce.next(a) }
             }
         }
     }
 }
 
-public class AurumListProvider<D: Equatable, C: UICollectionViewCell>{
-    let list: UICollectionView
+public class AurumCollectionProvider<Data: Equatable, Cell: UICollectionViewCell, A: AurumAction>{
+    public typealias Reducer = Subject<A, Never>
+    public typealias Setuper = (Cell, Data, [Data], Int, Reducer) -> Void
+
+    let collection: UICollectionView
     let size: CGSize
-    let setup: (C, D, [D], IndexPath) -> Void
+    let setup: Setuper
     
-    public init(list: UICollectionView, size: CGSize, setup: @escaping (C, D, [D], IndexPath) -> Void) {
-        self.list = list
+    public init(_ collection: UICollectionView, size: CGSize, setup: @escaping Setuper) {
+        self.collection = collection
         self.size = size
         self.setup = setup
     }
 }
 
-//Create binding Data -> (Collection, Connector)
-public func *><S: AurumState, LC: AurumListConnector, A: AurumAction>(left: @escaping (S) -> [LC.DataType], right: (UICollectionView, LC)) -> AurumListBinding<S, LC, A>{
+public class AurumTableProvider<Data: Equatable, Cell: UITableViewCell, A: AurumAction>{
+    public typealias Reducer = Subject<A, Never>
+    public typealias Setuper = (Cell, Data, [Data], Int, Reducer) -> Void
+
+    let table: UITableView
+    let setup: Setuper
+    
+    public init(_ table: UITableView, setup: @escaping Setuper) {
+        self.table = table
+        self.setup = setup
+    }
+}
+
+//Create binding: Data -> (Collection, Connector)
+//Collection
+public func *><S: AurumState, LC: AurumCollectionConnector>(left: @escaping (S) -> [LC.Data], right: (UICollectionView, LC)) -> AurumListBinding<S, LC>{
     right.0.connector = right.1
-    right.1.collectionView = right.0
-    return AurumListBinding(extractor: left, component: right.0, connector: right.1)
+    right.1.reloadList = right.0.reloadData
+    return AurumListBinding(extractor: left, connector: right.1)
 }
 
-public func *><S: AurumState, D: Equatable, C: UICollectionViewCell, A: AurumAction>(left: @escaping (S) -> [D], right: AurumListProvider<D, C>) -> AurumListBinding<S, AurumListBaseConnector<D, C>, A>{
-    let connector = AurumListBaseConnector(setuper: right.setup)
-    right.list.connector = connector
-    connector.collectionView = right.list
-    return AurumListBinding(extractor: left, component: right.list, connector: connector)
+public func *><S: AurumState, D: Equatable, C: UICollectionViewCell, A: AurumAction>(left: @escaping (S) -> [D], right: AurumCollectionProvider<D, C, A>) -> AurumListBinding<S, AurumCollectionBaseConnector<D, C, A>>{
+    let connector = AurumCollectionBaseConnector(size: right.size, setuper: right.setup)
+    right.collection.connector = connector
+    connector.reloadList = right.collection.reloadData
+    return AurumListBinding(extractor: left, connector: connector)
 }
 
+//Table
+public func *><S: AurumState, LC: AurumTableConnector>(left: @escaping (S) -> [LC.Data], right: (UITableView, LC)) -> AurumListBinding<S, LC>{
+    right.0.connector = right.1
+    right.1.reloadList = right.0.reloadData
+    return AurumListBinding(extractor: left, connector: right.1)
+}
 
-//Create full binding (Data -> (Collection, Connector)) -> Action
-public func *><S: AurumState, LC: AurumListConnector, A: AurumAction>(left: AurumListBinding<S, LC, A>, right: @escaping (IndexPath) -> A?) -> AurumBinding<S, A>{
+public func *><S: AurumState, D: Equatable, C: UITableViewCell, A: AurumAction>(left: @escaping (S) -> [D], right: AurumTableProvider<D, C, A>) -> AurumListBinding<S, AurumTableBaseConnector<D, C, A>>{
+    let connector = AurumTableBaseConnector(setuper: right.setup)
+    right.table.connector = connector
+    connector.reloadList = right.table.reloadData
+    return AurumListBinding(extractor: left, connector: connector)
+}
+
+//Create full binding: ListBinding -> Action
+public func *><S: AurumState, LC: AurumListConnector>(left: AurumListBinding<S, LC>, right: @escaping (Int) -> LC.Action?) -> AurumBinding<S, LC.Action>{
     left.action = right
     return left
 }
 
-struct AurumAurumListConnectorKeys {
-    static var CollectionView = "CollectionView"
-    static var Items = "Items"
-    static var ActionListener = "ActionListener"
-}
-
-public extension AurumListConnector{
-    var collectionView: UICollectionView! {
-        get {
-            if let collection = objc_getAssociatedObject(self, &AurumAurumListConnectorKeys.CollectionView) as? UICollectionView{ return collection }
-            return nil
-        }
-        set {
-            objc_setAssociatedObject(self, &AurumAurumListConnectorKeys.CollectionView, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-    
-    var actionListener: AurumListActionListener?{
-        get {
-            if let listener = objc_getAssociatedObject(self, &AurumAurumListConnectorKeys.ActionListener) as? AurumListActionListener{ return listener }
-            return nil
-        }
-        set {
-            objc_setAssociatedObject(self, &AurumAurumListConnectorKeys.ActionListener, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-    
-    func set(actionListener: @escaping (IndexPath) -> Void){
-        self.actionListener = AurumListActionListener(actionListener: actionListener)
-    }
-}
-
-struct AurumUICollectionViewKeys {
-    static var Connector = "Connector"
-}
-
 public extension UICollectionView{
+    struct AurumUICollectionViewKeys {
+        static var Connector = "Connector"
+    }
+
     var connector: NSObjectProtocol?{
-        get {
-            if let connector = objc_getAssociatedObject(self, &AurumUICollectionViewKeys.Connector) as? NSObject{ return connector }
-            return nil
-        }
+        get { return objc_getAssociatedObject(self, &AurumUICollectionViewKeys.Connector) as? NSObject }
         set {
             objc_setAssociatedObject(self, &AurumUICollectionViewKeys.Connector, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            delegate = newValue as? UICollectionViewDelegateFlowLayout
+            dataSource = newValue as? UICollectionViewDataSource
+        }
+    }
+}
+
+public extension UITableView{
+    struct AurumUITableViewKeys {
+        static var Connector = "Connector"
+    }
+
+    var connector: NSObjectProtocol?{
+        get { return objc_getAssociatedObject(self, &AurumUITableViewKeys.Connector) as? NSObject }
+        set {
+            objc_setAssociatedObject(self, &AurumUITableViewKeys.Connector, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            delegate = newValue as? UITableViewDelegate
+            dataSource = newValue as? UITableViewDataSource
         }
     }
 }
